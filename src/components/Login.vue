9b1829b835ff790516f451d2021fcdba3ccf6475
zhu5317460 <zhutongbin@aliyun.com>
2019-04-19 20:00:32 +0800

    删除不需要的文件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-  </div>
+  <h1>登录页</h1>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Login'
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
