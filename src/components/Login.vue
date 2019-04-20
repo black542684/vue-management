4181cd13e437f30f2481a843c20aea7e6ccafc31
zhu5317460 <zhutongbin@aliyun.com>
2019-04-20 19:14:54 +0800

    登录页的表单验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,55 @@
 <template>
-  <div class="container">
-    <div class="centre"></div>
+  <div class="login-container">
+    <div class="centre">
+      <img src="../assets/images/logo.png" height="56" width="207"/>
+      <el-form :rules="rules" :model="form">
+        <el-form-item prop="username">
+          <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input placeholder="请输入密码" type="password" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">登录</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '密码长度在 6 到 18 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  }
 };
 </script>
 
 <style scoped>
-.container{
+.login-container{
   height: 100%;
   background:linear-gradient(45deg,#EFF7FE,#0C5460);
 }
-.container .centre{
+.login-container .centre{
   width: 407px;
   height: 255px;
+  padding:15px;
   border:1px solid #ccc;
   box-shadow:0 2px 15px #eee;
   border-radius:5px;
@@ -26,5 +58,11 @@ export default {
   left:50%;
   transform:translate(-50%,-60%);
   background:linear-gradient(210deg,#f0f8ff,#d1ecf1);
+  box-sizing:border-box;
+}
+.login-container .centre img{
+  display:block;
+  margin:0 auto;
+  margin-bottom:15px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import './assets/css/global.css';
+// 导入element-ui和静态资源
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
-
+// 设置element-ui的全局样式 大小设置为 小
+Vue.use(ElementUI, {size: 'small'});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
